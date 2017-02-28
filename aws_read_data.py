79d46c24f0de22b61ba07585df37a1e4c3db3314
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-02-28 15:26:54 -0700

    Updated CNN file

--- aws_read_data.py
+++ aws_read_data.py
@@ -1,6 +1,30 @@
+import boto
 import cPickle
 import numpy as np
 
+def s3_upload_files(bucketname, *args):
+    """With the first string as the name of a bucket on s3, upload each individual
+    file from the filepaths listed in the list of strings.
+    Parameters
+    ----------
+    bucketname: String, List of Strings
+    *args: Strings, each representing a filepath to upload.
+    Returns
+    -------
+    None. Side effect is files will be uploaded.
+    """
+    access_key, secret_access_key = get_aws_access()
+    conn = boto.connect_s3(access_key, secret_access_key)
+
+    if conn.lookup(bucket_name) is None:
+        bucket = conn.create_bucket(bucket_name, policy='public-read')
+    else:
+        bucket = conn.get_bucket(bucket_name)
+
+    for filename in args:
+            key = bucket.new_key(filename)
+            key.set_contents_from_filename(filename)
+            
 if __name__ == '__main__':
     data_url = 'https://s3.amazonaws.com/capproj2017/X_arr_100.pkl'
     X = cPickle.load(open(data_url))
