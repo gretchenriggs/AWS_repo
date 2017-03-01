0b0298704e8592ae6e7a1353166331b0b37bbbc0
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-03-01 13:17:05 -0700

    Making sure temp push deleted

--- aws_read_data.py
+++ aws_read_data.py
@@ -1,6 +1,9 @@
-import boto
 import cPickle
 import numpy as np
+import boto
+import os
+access_key = os.environ['AWS_ACCESS_KEY']
+secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
 def s3_upload_files(bucketname, *args):
     """With the first string as the name of a bucket on s3, upload each individual
@@ -24,9 +27,33 @@ def s3_upload_files(bucketname, *args):
     for filename in args:
             key = bucket.new_key(filename)
             key.set_contents_from_filename(filename)
-            
+
+# Create connection to s3 and connect to the bucket
+conn = boto.connect_s3(access_key, secret_access_key)
+b = conn.get_bucket(bucket)
+all_buckets = [b.name for b in conn.get_all_buckets()]
+
+bucket = 'capproj2017'
+
+local_file = '/tmp/X_arr_100.pkl'
+bucket.get_key(aws_app_assets + "X_arr_100.pkl") \
+      .get_contents_to_filename(local_file)
+clf = joblib.load(local_file)
+os.remove(local_file)
+
+
+if conn.lookup(bucket) is None:
+    b = conn.create_bucket(bucket, policy='public-read')
+else:
+    b = conn.get_bucket(bucket)
+
+file_object = b.new_key('X_arr_100.pkl')
+file_object.set_contents_from_string('X_arr_100.pkl, policy='public-read')
+
+key.get_contents_to_filename(os.path.join(path, key.name))
+
 if __name__ == '__main__':
-    data_url = 'https://s3.amazonaws.com/capproj2017/X_arr_100.pkl'
+    data_url = 'X_arr_100.pkl'
     X = cPickle.load(open(data_url))
     X = np.asarray(X)
 
