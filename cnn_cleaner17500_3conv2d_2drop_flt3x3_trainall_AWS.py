d5ca688cb2271a2def90d1d9247ff4e4c1633944
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-03-02 12:56:40 -0700

    Fixed typo

--- cnn_cleaner17500_3conv2d_2drop_flt3x3_trainall_AWS.py
+++ cnn_cleaner17500_3conv2d_2drop_flt3x3_trainall_AWS.py
@@ -248,7 +248,7 @@ if __name__ == '__main__':
     # Load in pickled 17,500 124x124x3 images from AWS S3 and labels (0, 1)
     features1 = 'X_arr_6700.pkl'
     features2 = 'X_arr_6700_deg0mir.pkl'
-    feature3 = 'X_arr_6700_deg90.pkl'
+    features3 = 'X_arr_6700_deg90.pkl'
     labels = 'Image_Labels_125x125_17500.txt'
     X1 = get_X_from_bucket(features1)
     X2 = get_X_from_bucket(features2)
