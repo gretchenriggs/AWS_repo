d2ee9946255f5322adc4e688566d5fcadc2261e3
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-03-10 13:07:59 -0700

    Updated grayscale portions of the code

--- cnn_cleaner53600_3conv2d_2drop_flt3x3_adad_bval_gray_AWS.py
+++ cnn_cleaner53600_3conv2d_2drop_flt3x3_adad_bval_gray_AWS.py
@@ -2,7 +2,7 @@
     objects (0) or containing some man-made objects (1).
     Using Theano with Tensorflow image_dim_ordering :
     (# images, # rows, # cols, # channels)
-    (53600, 124, 124, 3) for the X images below
+    (53600, 124, 124, 1) for the X images below
 '''
 import cPickle
 import resource
@@ -56,22 +56,22 @@ def preproc(X_train, X_test):
     '''
     # Converting RGB to Gray Scale Images to train for Moon/Mars prediction test
     # The pixel values after converting to Gray Scale are between 0 & 1
+    X_train_gray = np.empty([0, 124, 124], dtype="float32")
+    X_test_gray = np.empty([0, 124, 124], dtype="float32")
     for i in xrange(len(X_train)):
-        X_train[i] = rgb2gray(X_train[i])
-        X_train[i] = X_train[i].astype("float32")
+        X_train_gray = np.insert(X_train_gray, 0, rgb2gray(X_train[i]), axis=0)
     for i in xrange(len(X_test)):
-        X_test[i] = rgb2gray(X_test[i])
-        X_test[i] = X_test[i].astype("float32")
+        X_test_gray = np.insert(X_test_gray, 0, rgb2gray(X_test[i]), axis=0)
 
     # Zero-center the data (important), in steps to prevent memory error
     # Compute mean only on Training data to prevent leakage of info into
     #   Test set
-    mean_X_train = np.mean(X_train)
-    for i in range(len(X_train)):
-        X_train[i] = X_train[i] - mean_X_train
-    for i in range(len(X_test)):
-        X_test[i] = X_test[i] - mean_X_train
-    return X_train, X_test
+    mean_X_train_gray = np.mean(X_train_gray)
+    for i in range(len(X_train_gray)):
+        X_train_gray[i] = X_train_gray[i] - mean_X_train_gray
+    for i in range(len(X_test_gray)):
+        X_test_gray[i] = X_test_gray[i] - mean_X_train_gray
+    return X_train_gray, X_test_gray
 
 
 def train_test(X, y, nb_classes, test_percent=0.20):
@@ -89,20 +89,21 @@ def train_test(X, y, nb_classes, test_percent=0.20):
     print(X_train.shape[0], 'train samples')
     print(X_test.shape[0], 'test samples')
 
-    # Calling function to set pixel values between 0-1 and center around zero
-    X_train, X_test = preproc(X_train, X_test)
+    # Calling function to set pixels to Gray Scale with values between 0-1
+    #  and center around zero.
+    X_train_gray, X_test_gray = preproc(X_train, X_test)
 
     # Convert class vectors to binary class matrices
     y_train = np_utils.to_categorical(y_train, nb_classes)
     y_test = np_utils.to_categorical(y_test, nb_classes)
 
-    return X_train, X_test, y_train, y_test
+    return X_train_gray, X_test_gray, y_train, y_test
 
 
-def keras_inp_prep(X_train, X_test, img_dep, img_rows, img_cols, \
+def keras_inp_prep(X_train_gray, X_test_gray, img_dep, img_rows, img_cols, \
                    dim_ordering='tf'):
-    ''' Reshape feature matrices X_train, X_test to be compatible with neural
-          network (NN) expected input_shape.
+    ''' Reshape feature matrices X_train_gray, X_test_gray to be compatible\
+          with neural network (NN) expected input_shape.
         if dim_ordering is th (Theano), NN expects:
             (# images, # channels, # rows, # cols)
         if dim_ordering is tf (TensorFlow), NN expects:
@@ -110,34 +111,34 @@ def keras_inp_prep(X_train, X_test, img_dep, img_rows, img_cols, \
         The tf and th image_dim_ordering is first set in the
             ~/keras/keras.json file.  Make sure it matches the
             K.set_image_dim_ordering at the beginning of this script.
-        Input: X_train, X_test - arrays
+        Input: X_train_gray, X_test_gray - arrays
                img_dep, img_rows, img_cols - integers
                dim_ordering - string
-        Output: X_train, X_test - arrays
+        Output: X_train_gray, X_test_gray - arrays
                 input_shape - tuple
     '''
     if K.image_dim_ordering() == 'th':
-        X_train = X_train.reshape(X_train.shape[0], img_dep, img_rows, \
-                                  img_cols)
-        X_test = X_test.reshape(X_test.shape[0], img_dep, img_rows, \
-                                img_cols)
+        X_train_gray = X_train_gray.reshape(X_train_gray.shape[0], img_dep,\
+                                            img_rows, img_cols)
+        X_test_gray = X_test_gray.reshape(X_test_gray.shape[0], img_dep,\
+                                          img_rows, img_cols)
         input_shape = (img_dep, img_rows, img_cols)
     else:
-        X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, \
-                                  img_dep)
-        X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, \
-                                img_dep)
+        X_train_gray = X_train_gray.reshape(X_train_gray.shape[0], img_rows,\
+                                            img_cols, img_dep)
+        X_test_gray = X_test_gray.reshape(X_test_gray.shape[0], img_rows,\
+                                          img_cols, img_dep)
         input_shape = (img_rows, img_cols, img_dep)
-    return X_train, X_test, input_shape
+    return X_train_gray, X_test_gray, input_shape
 
 
-def cnn(X_train, y_train, X_test, y_test, kernel_size, pool_size,\
+def cnn(X_train_gray, y_train, X_test_gray, y_test, kernel_size, pool_size,\
         batch_size, nb_classes, nb_epoch):
     ''' This is the Convolutional Neural Net architecture, which is the
             workhorse in this python script, for classifying the images
             as natural (0) or containing man-made images (1).
-        Input: X_train, y_train (training data)- arrays
-               X_test, y_test (cross-validation data) - arrays
+        Input: X_train_gray, y_train (training data)- arrays
+               X_test_gray, y_test (cross-validation data) - arrays
                kernel_size (filter size) - integer
                pool_size (desampling values) - tuple of integers
                batch_size (# of images to process at a time for gradient
@@ -153,7 +154,7 @@ def cnn(X_train, y_train, X_test, y_test, kernel_size, pool_size,\
     # 2 Convolutional Layers with RELU activations applied
     #   followed by a 2x2 MaxPooling Layer and 50% Dropout
     model.add(Convolution2D(32, kernel_size, kernel_size, \
-                            border_mode='valid', input_shape=X_train.shape[1:]))
+                            border_mode='valid', input_shape=X_train_gray.shape[1:]))
     model.add(Activation('relu'))
     model.add(Convolution2D(32, kernel_size, kernel_size))
     model.add(Activation('relu'))
@@ -183,27 +184,27 @@ def cnn(X_train, y_train, X_test, y_test, kernel_size, pool_size,\
 
     # Training the model, validating results with 'accuracy' metric during
     #   training on both train and test sets for each epoch
-    model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-              verbose=1, validation_data=(X_test, y_test))
+    model.fit(X_train_gray, y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              verbose=1, validation_data=(X_test_gray, y_test))
 
     # Evaluating final accuracy on training and test sets
-    score = model.evaluate(X_test, y_test, verbose=0)
+    score = model.evaluate(X_test_gray, y_test, verbose=0)
     print('Test accuracy:', score[1])
-    score_train = model.evaluate(X_train, y_train, verbose=0)
+    score_train = model.evaluate(X_train_gray, y_train, verbose=0)
     print('Train accuracy:', score_train[1])
 
     return model, score, score_train
 
 
-def model_performance(model, X_train, X_test, y_train, y_test):
+def model_performance(model, X_train_gray, X_test_gray, y_train, y_test):
     ''' Compute accuracy, precision, recall, F1-Score
         Output the predicted y_test & y_train values
         Output the probabilities for the predicted y_test & y_train values
         Output confusion matrix for y_test
         Input: model - Sequential object
-               X_train - array
+               X_train_gray - array
                y_train - array
-               X_test - array
+               X_test_gray - array
                y_test - array
         Output: y_train_pred - array
                 y_test_pred - array
@@ -212,12 +213,12 @@ def model_performance(model, X_train, X_test, y_train, y_test):
                 conf_matrix, array
     '''
     # Predictions on Test and Train datasets
-    y_test_pred = model.predict_classes(X_test)
-    y_train_pred = model.predict_classes(X_train)
+    y_test_pred = model.predict_classes(X_test_gray)
+    y_train_pred = model.predict_classes(X_train_gray)
 
     # Prediction's probability outputs for Test & Train datasets
-    y_test_pred_proba = model.predict_proba(X_test)
-    y_train_pred_proba = model.predict_proba(X_train)
+    y_test_pred_proba = model.predict_proba(X_test_gray)
+    y_train_pred_proba = model.predict_proba(X_train_gray)
 
     # Converting y_test back to 1-D array for confusion matrix computation
     y_test_1d = y_test[:,1]
@@ -248,7 +249,14 @@ if __name__ == '__main__':
     features4 = 'X_arr_6700_deg90mir.pkl'
     features5 = 'X_arr_6700_deg180.pkl'
     features6 = 'X_arr_6700_deg180mir.pkl'
-    features7 = 'X_arr_6700_deg270.pkl'
+    features7 = 'X_arr_6700_deg270.pkl'    batch_size = 32
+    nb_classes = 2
+    nb_epoch = 20
+    kernel_size = 3
+    pool_size = (2, 2)
+
+    # Train/test split for cross validation
+    test_percent = 0.20
     features8 = 'X_arr_6700_deg270mir.pkl'
     labels = 'Image_Labels_125x125_53600.txt'
     X1 = get_X_from_bucket(features1)
@@ -269,24 +277,31 @@ if __name__ == '__main__':
     kernel_size = 3
     pool_size = (2, 2)
 
-    # Train/test split for cross validation
+    # Train/test split for cross validation and making Grayscale
     test_percent = 0.20
-    X_train, X_test, y_train, y_test, y_train_orig, y_test_orig = \
+    X_train_gray, X_test_gray, y_train, y_test, y_train_orig, y_test_orig = \
                             train_test(X, y, nb_classes, test_percent)
 
-    # input image dimensions - 124x124x3 for input RGB Satellite Images
-    img_rows, img_cols, img_dep = X_train.shape[1], X_train.shape[2], \
-                                  X_train.shape[3]
+    # Reshape X_train_gray & X_test_gray from
+    #   Nx124x124 to Nx124x124x1, where N=number of images
+    X_train_gray = np.reshape(X_train_gray, (X_train_gray.shape[0], \
+                              X_train_gray.shape[1], X_train_gray.shape[2], 1))
+    X_test_gray = np.reshape(X_test_gray, (X_test_gray.shape[0], \
+                              X_test_gray.shape[1], X_test_gray.shape[2], 1))
+
+    # Input image dimensions - 124x124x1 for input Grayscale Satellite Images
+    img_rows, img_cols, img_dep = X_train_gray.shape[1], X_train_gray.shape[2],\
+                                      X_train_gray.shape[3]
 
     # Prep input for Keras
     # For Tensorflow dim_ordering (tf), (# images, # rows, # cols, # chans)
     # For Theano dim ordering (th), (# images, # chans, # rows, # cols)
     # Used Theano backend, tf dim_ordering for this project
-    X_train, X_test, input_shape = keras_inp_prep(X_train, X_test,\
-                       img_dep, img_rows, img_cols, dim_ordering='tf')
+    X_train_gray, X_test_gray, input_shape = keras_inp_prep(X_train_gray,\
+                X_test_gray, img_dep, img_rows, img_cols, dim_ordering='tf')
 
     # Run Convolutional Neural Net
-    model, score, score_train = cnn(X_train, y_train, X_test, y_test,\
+    model, score, score_train = cnn(X_train_gray, y_train, X_test_gray, y_test,\
                         kernel_size, pool_size, batch_size, nb_classes,\
                         nb_epoch)
 
@@ -295,7 +310,7 @@ if __name__ == '__main__':
 
     # Evaluating CNN Model performance
     y_train_pred, y_test_pred, y_train_pred_proba, y_test_pred_proba, \
-       conf_matrix = model_performance(model, X_train, X_test, y_train, y_test)
+       conf_matrix = model_performance(model, X_train_gray, X_test_gray, y_train, y_test)
     accuracy = float(conf_matrix[0][0] + conf_matrix[1][1]) \
                      / np.sum(conf_matrix)
     precision = float(conf_matrix[0][0]) \
